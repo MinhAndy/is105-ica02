48c28d259f6db25e42d83eb84bf9cdb1b719eccc
Andy <minh_andy@outlook.com>
2019-04-24 00:55:50 +0200

    dette er slice.go

--- slice/slice.go
+++ slice/slice.go
@@ -1,24 +1,20 @@
 package slice
 
-//import "fmt"
-
-// AllocateVar har INN-argument b
-// b - antall bytes brukeren ønsker å allokere 
-// Returnerer en slice av type []byte
-// 
 func AllocateVar(b int) []byte {
-	// Kode for Oppgave 5a
+	return []byte{}
 }
 
-// AllocateMake tar lengde og kapasitet som b og lager en ny slice
-//
 func AllocateMake(b int) []byte {
-	// Kode for Oppgave 5a
+	return make([]byte, b)
 }
 
-// Reslice takes a slice and reslices it
 func Reslice(slc []byte, lidx int, uidx int) []byte {
-	// kode her for 5b
+	s := CopySlice(slc)
+	return s[lidx:uidx]
 }
 
-// CopySlice ???
+func CopySlice(slc []byte) []byte {
+	s := AllocateMake(len(slc))
+	copy(s, slc)
+	return s
+}
